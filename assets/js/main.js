1546be849af93fc46538119a78d144179881ce69
WelissonSC <welissonsilvacunha@gmail.com>
2024-10-18 09:16:02 -0300

    atualizaçao de escopo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,29 @@
+escopo = () => {
+
+    //pegando os elementos do html
 const texto = document.querySelector('.texto');
 const lista = document.querySelector('.tarefas');
 
+//função que cria uma ul e retorna a mesma
 criaUl = () => {
     const li = document.createElement('li');
     return li;
 }
 
+//funcoa que cria um botão já setando uma classe 
 criaBtnDelete = () => {
     const btnDelete = document.createElement('button')
     btnDelete.setAttribute('class', 'remove');
     return btnDelete;
 }
 
+//limpa o campo de texto após a digitação do usuário
 limpaCampo = () => {
     texto.value = '';
     texto.focus();
 }
 
+//insera as tarefas no documento após clicar no botão de adicionar
 insereNoDocumento = (texto) => {
     const li = criaUl();
     const btnRemove = criaBtnDelete();
@@ -27,6 +34,7 @@ insereNoDocumento = (texto) => {
     limpaCampo();
 }
 
+//essa função salva na propria página atravez do JSON as tarefas que ficaram salvas 
 salvaTarefas = () => {
     const allTarefas = lista.querySelectorAll('li');
     const listTarefas = [];
@@ -36,11 +44,14 @@ salvaTarefas = () => {
         tarefaTxt = tarefaTxt.replace('Remover', '')
         listTarefas.push(tarefaTxt);
     }
+    
     const tarefasJSON = JSON.stringify(listTarefas);
     localStorage.setItem('tarefas', tarefasJSON );
     console.log(tarefasJSON);
 }
 
+
+//pega as tarefas em formato JSON tras de volta para html e insere na página
 addTarefaSalva = () => {
     const tarefas = localStorage.getItem('tarefas')
     const listTarefas = JSON.parse(tarefas);
@@ -85,6 +96,11 @@ document.addEventListener('click', (event) => {
     }
 
 });
+
+}
+
+escopo()
+
 
 
 
